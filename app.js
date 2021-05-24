ed3d1dfd30cdd4c4de9df7cdf001db8bd152f3ae
Scott Rosenquist <scott.rosenquist@mercatus.com>
2021-05-23 20:46:47 -0400

    Got it working

--- app.js
+++ app.js
@@ -1,43 +1,149 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const express = require('express')
+const pg = require('pg')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express()
+// configs come from standard PostgreSQL env vars
+// https://www.postgresql.org/docs/9.6/static/libpq-envars.html
+const pool = new pg.Pool()
 
-var app = express();
+const queryHandler = (req, res, next) => {
+  pool.query(req.sqlQuery).then((r) => {
+    return res.json(r.rows || [])
+  }).catch(next)
+}
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/', (req, res) => {
+  res.send('Welcome to EQ Works 😎')
+})
 
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/events/hourly', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, hour, events
+    FROM public.hourly_events
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/events/daily', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, SUM(events) AS events
+    FROM public.hourly_events
+    GROUP BY date
+    ORDER BY date
+    LIMIT 7;
+  `
+  return next()
+}, queryHandler)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.get('/stats/hourly', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, hour, impressions, clicks, revenue
+    FROM public.hourly_stats
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/stats/daily', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date,
+        SUM(impressions) AS impressions,
+        SUM(clicks) AS clicks,
+        SUM(revenue) AS revenue
+    FROM public.hourly_stats
+    GROUP BY date
+    ORDER BY date
+    LIMIT 7;
+  `
+  return next()
+}, queryHandler)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/poi', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT *
+    FROM public.poi;
+  `
+  return next()
+}, queryHandler)
 
-module.exports = app;
+app.listen(process.env.PORT || 5555, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log(`Running on ${process.env.PORT || 5555}`)
+  }
+})
+
+// last resorts
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception: ${err}`)
+  process.exit(1)
+})
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
+  process.exit(1)
+})
+
+module.exports = app
+
+//var createError = require('http-errors');
+//const express = require('express')
+//var path = require('path');
+//var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
+//var cors = require('cors');
+//const pg = require('pg')
+//
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+//
+//const app = express()
+//
+//const pool = new pg.Pool()
+//
+//const queryHandler = (req, res, next) => {
+//  if (!res.finished) {
+//    pool
+//      .query(req.sqlQuery)
+//      .then(r => {
+//        return res.json(r.rows || []);
+//      })
+//      .catch(next);
+//  }
+//};
+//
+//// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+//
+//app.use(cors());
+//app.use(logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+//
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+//
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  next(createError(404));
+//});
+//
+//// error handler
+//app.use(function(err, req, res, next) {
+//  // set locals, only providing error in development
+//  res.locals.message = err.message;
+//  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//  // render the error page
+//  res.status(err.status || 500);
+//  res.render('error');
+//});
+//
+//module.exports = app;
