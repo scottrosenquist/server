837c85a17ecedcfc8a0b4657714616797159ebcc
Scott Rosenquist <scott.rosenquist@mercatus.com>
2021-05-31 00:54:51 -0400

    Tidy up

--- rate-limiter.js
+++ rate-limiter.js
@@ -1,20 +1,16 @@
-const Redis = require("ioredis")
-
-console.log('process.env.REDIS_URL: ' + process.env.REDIS_URL )
+const Redis = require('ioredis')
 
 const redis = new Redis(process.env.REDIS_URL)
 
 const rateLimiter = (req, res, next) => {
   try {
     if (!redis) {
-      throw new Error("Redis client not found")
+      throw new Error('Redis client not found')
       process.exit(1)
     }
     var id = req.ip
-    console.log(id)
     redis.get(id, (err, record) => {
       if (err) throw err
-      console.log(record)
 
       if (record == null || record < 5) {
         redis
@@ -23,7 +19,7 @@ const rateLimiter = (req, res, next) => {
           .expire(id, 5)
           .exec()
       } else {
-        res.status(429).json({ error: "Too many requests sent" })
+        res.status(429).json({ error: 'Too many requests sent' })
       }
       next()
     })
