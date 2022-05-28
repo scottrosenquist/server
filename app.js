9472958792b5ef0e017ba6d76c832570922dcf56
Scott Rosenquist <scott.rosenquist@mercatus.com>
2022-05-27 21:17:57 -0400

    wip

--- app.js
+++ app.js
@@ -26,110 +26,190 @@ app.use(cors());
 
 app.use(rateLimiter)
 
+
 app.get('/events/monthly', (req, res, next) => {
   req.sqlQuery = `
-    SELECT TO_CHAR(DATE_TRUNC('month', date),'Mon') AS month,
-           SUM(events) AS events, name
-    FROM public.hourly_events, public.poi
-    WHERE public.hourly_events.poi_id = public.poi.poi_id
-    GROUP BY poi.name, month, hourly_events.date
-    ORDER BY date, poi.name
-    LIMIT 168;
-  `
-  return next()
-}, queryHandler)
-
-app.get('/events/week', (req, res, next) => {
-  req.sqlQuery = `
-    SELECT date, json_object_agg(name, events) AS data
-    FROM (
-      SELECT date, name,sum(events) AS events
-      FROM public.hourly_events, public.poi
-      WHERE public.hourly_events.poi_id = public.poi.poi_id
-      GROUP BY date, poi.name
-      ORDER BY date
-      LIMIT 168
-    ) AS derivedTable
-    GROUP BY derivedTable.date
-    LIMIT 7
-  `
-  return next()
-}, queryHandler)
-
-app.get('/events/poi', (req, res, next) => {
-  req.sqlQuery = `
-    SELECT date, hour, events, name
-    FROM public.hourly_events, public.poi
-    WHERE public.hourly_events.poi_id = public.poi.poi_id
-    ORDER BY date, hour
-    LIMIT 168;
-  `
-  return next()
-}, queryHandler)
-
-app.get('/events/hourly', (req, res, next) => {
-  req.sqlQuery = `
-    SELECT date, hour, events
-    FROM public.hourly_events
-    ORDER BY date, hour
-    LIMIT 168;
+    SELECT date_trunc('month', date) AS date,
+           sum(events) AS events,
+           poi.poi_id,
+           name AS poi_name
+    FROM hourly_events,
+         poi
+    WHERE hourly_events.poi_id = poi.poi_id
+    GROUP BY date_trunc('month', date),
+             poi.poi_id
+    ORDER BY date_trunc('month', date),
+             poi.poi_id
   `
   return next()
 }, queryHandler)
 
 app.get('/events/daily', (req, res, next) => {
   req.sqlQuery = `
-    SELECT date, SUM(events) AS events
-    FROM public.hourly_events
-    GROUP BY date
-    ORDER BY date
-    LIMIT 7;
-  `
-  return next()
-}, queryHandler)
-
-app.get('/stats/hourlypoi', (req, res, next) => {
-  req.sqlQuery = `
-    SELECT date + interval '1 hour' * hour AS date, name, impressions, clicks, revenue
-    FROM public.hourly_stats, public.poi
-    WHERE public.hourly_stats.poi_id = public.poi.poi_id
-    ORDER BY date, hour
-    LIMIT 168;
-  `
-  return next()
-}, queryHandler)
-
-app.get('/stats/hourly', (req, res, next) => {
-  req.sqlQuery = `
-    SELECT date, hour, impressions, clicks, revenue
-    FROM public.hourly_stats
-    ORDER BY date, hour
-    LIMIT 168;
-  `
-  return next()
-}, queryHandler)
-
-app.get('/stats/daily', (req, res, next) => {
-  req.sqlQuery = `
     SELECT date,
-        SUM(impressions) AS impressions,
-        SUM(clicks) AS clicks,
-        SUM(revenue) AS revenue
-    FROM public.hourly_stats
-    GROUP BY date
-    ORDER BY date
-    LIMIT 7;
+           sum(events) AS events,
+           poi.poi_id,
+           name AS poi_name
+    FROM hourly_events,
+         poi
+    WHERE hourly_events.poi_id = poi.poi_id
+    GROUP BY date,
+             poi.poi_id
+    ORDER BY date,
+             poi.poi_id
   `
   return next()
 }, queryHandler)
 
-app.get('/poi', (req, res, next) => {
+app.get('/events/hourly', (req, res, next) => {
   req.sqlQuery = `
-    SELECT *
-    FROM public.poi;
+    SELECT date + interval '1 hour' * hour AS date,
+           events,
+           poi.poi_id,
+           name AS poi_name
+    FROM hourly_events,
+         poi
+    WHERE hourly_events.poi_id = poi.poi_id
+    ORDER BY date,
+             poi.poi_id
   `
   return next()
 }, queryHandler)
+
+app.get('/', (req, res, next) => {
+  req.sqlQuery = `
+  `
+  return next()
+}, queryHandler)
+
+app.get('/', (req, res, next) => {
+  req.sqlQuery = `
+  `
+  return next()
+}, queryHandler)
+
+app.get('/', (req, res, next) => {
+  req.sqlQuery = `
+  `
+  return next()
+}, queryHandler)
+
+app.get('/', (req, res, next) => {
+  req.sqlQuery = `
+  `
+  return next()
+}, queryHandler)
+
+app.get('/', (req, res, next) => {
+  req.sqlQuery = `
+  `
+  return next()
+}, queryHandler)
+
+//app.get('/events/monthly', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT TO_CHAR(DATE_TRUNC('month', date),'Mon') AS month,
+//           SUM(events) AS events, name
+//    FROM public.hourly_events, public.poi
+//    WHERE public.hourly_events.poi_id = public.poi.poi_id
+//    GROUP BY poi.name, month, hourly_events.date
+//    ORDER BY date, poi.name
+//    LIMIT 168;
+//  `
+//  return next()
+//}, queryHandler)
+//
+//app.get('/events/week', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT date, json_object_agg(name, events) AS data
+//    FROM (
+//      SELECT date, name,sum(events) AS events
+//      FROM public.hourly_events, public.poi
+//      WHERE public.hourly_events.poi_id = public.poi.poi_id
+//      GROUP BY date, poi.name
+//      ORDER BY date
+//      LIMIT 168
+//    ) AS derivedTable
+//    GROUP BY derivedTable.date
+//    LIMIT 7
+//  `
+//  return next()
+//}, queryHandler)
+//
+//app.get('/events/poi', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT date, hour, events, name
+//    FROM public.hourly_events, public.poi
+//    WHERE public.hourly_events.poi_id = public.poi.poi_id
+//    ORDER BY date, hour
+//    LIMIT 168;
+//  `
+//  return next()
+//}, queryHandler)
+//
+//app.get('/events/hourly', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT date, hour, events
+//    FROM public.hourly_events
+//    ORDER BY date, hour
+//    LIMIT 168;
+//  `
+//  return next()
+//}, queryHandler)
+//
+//app.get('/events/daily', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT date, SUM(events) AS events
+//    FROM public.hourly_events
+//    GROUP BY date
+//    ORDER BY date
+//    LIMIT 7;
+//  `
+//  return next()
+//}, queryHandler)
+//
+//app.get('/stats/hourlypoi', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT date + interval '1 hour' * hour AS date, name, impressions, clicks, revenue
+//    FROM public.hourly_stats, public.poi
+//    WHERE public.hourly_stats.poi_id = public.poi.poi_id
+//    ORDER BY date, hour
+//    LIMIT 168;
+//  `
+//  return next()
+//}, queryHandler)
+//
+//app.get('/stats/hourly', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT date, hour, impressions, clicks, revenue
+//    FROM public.hourly_stats
+//    ORDER BY date, hour
+//    LIMIT 168;
+//  `
+//  return next()
+//}, queryHandler)
+//
+//app.get('/stats/daily', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT date,
+//        SUM(impressions) AS impressions,
+//        SUM(clicks) AS clicks,
+//        SUM(revenue) AS revenue
+//    FROM public.hourly_stats
+//    GROUP BY date
+//    ORDER BY date
+//    LIMIT 7;
+//  `
+//  return next()
+//}, queryHandler)
+//
+//app.get('/poi', (req, res, next) => {
+//  req.sqlQuery = `
+//    SELECT *
+//    FROM public.poi;
+//  `
+//  return next()
+//}, queryHandler)
 
 app.listen(process.env.PORT || 5555, (err) => {
   if (err) {
